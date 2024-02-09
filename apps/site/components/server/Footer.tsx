4316b32f2309fb9434e90968bf71364cfbe79e1f
0xTranqui <thinkandchill11@gmail.com>
2024-02-09 13:51:42 -0500

    fix font size of footer

--- apps/site/components/server/Footer.tsx
+++ apps/site/components/server/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <Flex className="absolute bottom-[20px] gap-2 w-full justify-start">
       <Link href="https://www.river.ph/channel/bafyreihuti6faf2z322aaodxs2gq4cvqoysraiz5gxjolmlwud3jemz2re"></Link>
-      <Typography className="hover:cursor-pointer text-[14px]">
+      <Typography className="hover:cursor-pointer">
         view on river.ph
       </Typography>
     </Flex>
