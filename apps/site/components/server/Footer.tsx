81149ba4799b8fd5c4739265b8ce51fc72357e16
0xTranqui <thinkandchill11@gmail.com>
2024-02-09 22:20:22 -0500

    cleanup

--- apps/site/components/server/Footer.tsx
+++ apps/site/components/server/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <Flex className="absolute bottom-[20px] gap-2 w-full justify-start">
       <a href="https://www.river.ph/channel/bafyreihuti6faf2z322aaodxs2gq4cvqoysraiz5gxjolmlwud3jemz2re">
         <Typography className="hover:cursor-pointer text-[12px]">
-          missmayhem was here
+          view on river.ph
         </Typography>
       </a>
     </Flex>
